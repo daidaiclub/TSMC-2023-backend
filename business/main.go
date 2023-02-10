47780fd9afeab9d23b2cf910890f5a47f363dfa8
jimmyhealer <yahing6066@gmail.com>
2023-02-10 19:55:19 +0800

    update

--- business/main.go
+++ business/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"bytes"
-	"net/http"
-	"time"
-	"io"
+	"database/sql"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"github.com/lib/pq"
+	_ "github.com/lib/pq"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 type Order struct {
@@ -17,16 +20,16 @@ type Order struct {
 }
 
 type Inventory struct {
-	Material uint64      `json:"material"`
-	Signature string  `json:"signature"`
+	Material  uint64 `json:"material"`
+	Signature string `json:"signature"`
 }
 
 type Record struct {
-	Location string    `json:"location"`
-	Time     time.Time `json:"timestamp"`
-	Material uint64       `json:"material"`
-	Signature string   `json:"signature"`
-	Data     Data      `json:"data"`
+	Location  string    `json:"location"`
+	Time      time.Time `json:"timestamp"`
+	Material  uint64    `json:"material"`
+	Signature string    `json:"signature"`
+	Data      Data      `json:"data"`
 }
 
 type Data struct {
@@ -36,9 +39,8 @@ type Data struct {
 	D uint64 `json:"d"`
 }
 
-
 // func OrderToByteArray(Order order) []byte {
-   
+
 // }
 
 func main() {
@@ -46,17 +48,15 @@ func main() {
 
 	api := r.Group("/api")
 
-	
 	api.POST("/order", func(c *gin.Context) {
-		
+
 		var order Order
 		c.BindJSON(&order)
 
-
 		values := map[string]interface{}{
-			"location": order.Location,
+			"location":  order.Location,
 			"timestamp": order.Time,
-			"data": order.Data,
+			"data":      order.Data,
 		}
 
 		jsonValue, _ := json.Marshal(values)
@@ -76,6 +76,7 @@ func main() {
 		sitemap, err := io.ReadAll(res.Body)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
 
 		sitemap_s := []byte(string(sitemap))
@@ -89,19 +90,46 @@ func main() {
 		Record.Data = order.Data
 
 		//todo 資料
-	
 
 		c.JSON(200, gin.H{
-			"message" : "success",
+			"message": "success",
 		})
 	})
 
 	api.GET("/record", func(c *gin.Context) {
+		SQL_HOST := os.Getenv("SQL_HOST")
+		SQL_PORT := os.Getenv("SQL_PORT")
+		log.Println(SQL_HOST)
+		log.Println(SQL_PORT)
+		db, err := sql.Open("postgres", "postgresql://user:123456@"+SQL_HOST+":"+SQL_PORT+"/tsmc_pg")
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
+		m := "Steel"
+		rows, err := db.Query("SELECT * FROM data WHERE material = $1", m)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		defer rows.Close()
+		materials := []string{}
+
+		for rows.Next() {
+			var material string
+			if err := rows.Scan(&material); err != nil {
+				materials = append(materials, material)
+			}
+		}
+		c.JSON(200, gin.H{
+			"message": "success",
+			"data":    materials,
+		})
 	})
 
 	api.GET("/report", func(c *gin.Context) {
-	
+
 	})
 
 	r.Run(":8100")
